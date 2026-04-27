ec8d863af019ab9afd300b774df20a145f22dc73
zhangyudongfeng-ctrl <zhangyudongfeng@gmail.com>
2026-04-27 22:29:38 +0900

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # 这题就相当于是多维二分查找
+        # 2次二分, 第一次二分粗略划定范围, 找在哪个行中寻找target, 第二次二分进行详细寻找target
+        m, n = len(matrix), len(matrix[0])
+        top, bot = 0, m - 1
+        target_row = -1
+        while top <= bot:
+            row = (top + bot) // 2
+            # 如果target > 这行最后一个数, 说明需要在下一行中寻找, 更新top
+            if target > matrix[row][-1]: top = row + 1
+            # 如果target < 这行第一个数, 说明需要在上一行中寻找, 更新bot
+            elif target < matrix[row][0]: bot = row - 1
+            # 否则, 找到结果, 直接break, 进行第二次二分
+            else: 
+                target_row = row 
+                break
+
+        # 因为第一次二分没有return, 只在找到时break了, 没有处理没找到时的情况
+        if top > bot:
+            return False
+        l, r = 0, n - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[target_row][m]: l = m + 1
+            elif target < matrix[target_row][m]: r = m - 1
+            else: return True 
+        return False  
+        
